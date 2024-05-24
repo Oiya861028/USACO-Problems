fe1cc0d7d2a8e0ec8c6a5cd90a1c14c17f4d93bb
yubinx <yubinxie222@gmail.com>
2024-05-23 21:49:40 -0400

    Modified HashMap to cache lowest and Highest tuition

--- CowCollege.java
+++ CowCollege.java
@@ -11,12 +11,42 @@ public class CowCollege {
         System.out.println("Maximum Tuition: ");
         String tuition = sc.nextLine();
         String[] tuitions = tuition.split("\\s");
-        HashMap<String, Integer> tuitionCounter = new HashMap<String,Integer>();
+        HashMapKeyCached<String, Integer> tuitionCounter = new HashMapKeyCached<String,Integer>();
         System.out.println(Arrays.toString(tuitions));
         for(String t:tuitions) {
             if(tuitionCounter.containsKey(t)) tuitionCounter.replace(t, tuitionCounter.get(t)+1);
             else tuitionCounter.put(t, 1);
         }
-        tuitionCounter.forEach((key, value) -> System.out.println(key + " " + value));
+        System.out.println("Lowest key_value pair:" + tuitionCounter.getlowestK()+"-"+tuitionCounter.get(tuitionCounter.getlowestK()));
+        System.out.println("Highest key_value pair:" + tuitionCounter.getHighestK()+"-"+tuitionCounter.get(tuitionCounter.getHighestK()));
+
+    }
+    public static void calculateRevenue(HashMap<String, Integer> tuition, int numCows){
+        HashMap<Integer,Integer> tally = new HashMap<Integer,Integer>();
+    }
+    @SuppressWarnings("rawtypes")
+    public static class HashMapKeyCached<K extends Comparable, V> extends HashMap<K, V> {
+        K lowestValueKey, highestValueKey;
+        @SuppressWarnings("unchecked")
+        @Override
+        public V put(K key, V value) {
+            if(lowestValueKey==null || highestValueKey == null) {
+                lowestValueKey = key;
+                highestValueKey = key;
+            }
+            else if(key.compareTo(lowestValueKey)<0) {
+                lowestValueKey = key;
+            }
+            else if(key.compareTo(highestValueKey)>0) {
+                highestValueKey = key;
+            }
+            return super.put(key, value);
+        }
+        public K getlowestK() {
+            return lowestValueKey;
+        }
+        public K getHighestK(){
+            return highestValueKey;
+        }
     }
 }
