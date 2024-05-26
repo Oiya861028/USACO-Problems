81c3abc2865e855ab40e98f32771cd6c61dd7595
yubinx <yubinxie222@gmail.com>
2024-05-25 21:02:03 -0400

    Complete calculateMaxRevenue by sorting and iterating through the cows

--- CowCollege.java
+++ CowCollege.java
@@ -1,7 +1,8 @@
-import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map.Entry;
 import java.util.Scanner;
-import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
 public class CowCollege {
     public static Scanner sc = new Scanner(System.in);
     public static void main(String[] args){ 
@@ -10,19 +11,36 @@ public class CowCollege {
         sc.nextLine();
         System.out.println("Maximum Tuition: ");
         String tuition = sc.nextLine();
-        String[] tuitions = tuition.split("\\s");
-        HashMapKeyCached<String, Integer> tuitionCounter = new HashMapKeyCached<String,Integer>();
-        System.out.println(Arrays.toString(tuitions));
-        for(String t:tuitions) {
+        String[] tuitionStr = tuition.split("\\s");
+        int[] tuitionArr = new int[numCows];
+        int k = 0;
+        for(String t:tuitionStr){
+            tuitionArr[k] = Integer.parseInt(t);
+            k++;
+        }
+        TreeMap<Integer, Integer> tuitionCounter = new TreeMap<Integer,Integer>();
+        for(int t:tuitionArr) {
             if(tuitionCounter.containsKey(t)) tuitionCounter.replace(t, tuitionCounter.get(t)+1);
             else tuitionCounter.put(t, 1);
         }
-        System.out.println("Lowest key_value pair:" + tuitionCounter.getlowestK()+"-"+tuitionCounter.get(tuitionCounter.getlowestK()));
-        System.out.println("Highest key_value pair:" + tuitionCounter.getHighestK()+"-"+tuitionCounter.get(tuitionCounter.getHighestK()));
-
+        calculateRevenue(tuitionCounter, numCows);
     }
-    public static void calculateRevenue(HashMap<String, Integer> tuition, int numCows){
-        HashMap<Integer,Integer> tally = new HashMap<Integer,Integer>();
+    public static void calculateRevenue(TreeMap<Integer, Integer> tuition, int numCows){
+        int highestTuition = 0;
+        int TuitionPerCow = 0;
+        for(Entry<Integer, Integer> n:tuition.entrySet()) {
+            int moneyPile = 0; //counting total tuition from charging certain rate
+            int tuitionFee = n.getKey(); //how much each cow paying
+            SortedMap<Integer,Integer> applicableCows =  tuition.subMap(tuitionFee, tuition.lastKey()+1);
+            for(Entry<Integer, Integer> cow: applicableCows.entrySet()) {
+                moneyPile += (tuitionFee*cow.getValue());
+            }
+            if(moneyPile>highestTuition) {
+                highestTuition = moneyPile;
+                TuitionPerCow = tuitionFee;
+            }
+        }
+        System.out.println(highestTuition + " " + TuitionPerCow);
     }
     @SuppressWarnings("rawtypes")
     public static class HashMapKeyCached<K extends Comparable, V> extends HashMap<K, V> {
